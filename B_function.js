16c88c9c7ef2ac076a31ea403fd90a00884a1d1d
bkashmere <43763979+bkashmere@users.noreply.github.com>
2018-11-16 15:42:56 -0800

    Create B_function.js

---
+++ B_function.js
@@ -0,0 +1,12 @@
+// NFL concussion data 
+
+var data = [{year: 2012, concussions: 265},
+            {year: 2013, concussions: 244},
+            {year: 2014, concussions: 212},
+            {year: 2015, concussions: 279},
+            {year: 2016, concussions: 250},
+            {year: 2017, concussions: 291}];
+            
+var barWidth = 40;
+var width = (barWidth + 10) * data.length;
+var height = 200;
