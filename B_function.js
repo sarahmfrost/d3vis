ab5bd9ae9e8b7f97f2cfa75d6ae82a2fc693e59b
bkashmere <43763979+bkashmere@users.noreply.github.com>
2018-11-16 15:48:10 -0800

    Update B_function.js

--- B_function.js
+++ B_function.js
@@ -10,3 +10,22 @@ var data = [{year: 2012, concussions: 265},
 var barWidth = 40;
 var width = (barWidth + 10) * data.length;
 var height = 200;
+
+var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
+var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.concussions; })]).
+  rangeRound([0, height]);
+
+var barDemo = d3.select("#bar-demo").
+  append("svg:svg").
+  attr("width", width).
+  attr("height", height);
+
+barDemo.selectAll("rect").
+  data(data).
+  enter().
+  append("svg:rect").
+  attr("x", function(datum, index) { return x(index); }).
+  attr("y", function(datum) { return height - y(datum.concussions); }).
+  attr("height", function(datum) { return y(datum.concussions); }).
+  attr("width", barWidth).
+  attr("fill", "#4286f4");
